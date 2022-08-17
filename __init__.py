5523eb0da8937bb508d436302c00c29eab096fe0
root <thin@dtu.dk>
2022-08-17 22:03:17 +0000

    Fixed some package imports

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 #from .wittyPi import clear_startup_time, clear_shutdown_time, getAll, schedule_file_lines2schedule_file_data, verify_schedule_data
-from .wittyPi import local_tz, wittyPiPath, is_schedule_file_in_use, schedule_file_lines2schedule_file_data, get_schedule_file, process_schedule_data, stringtime2timetuple, calcTime, set_shutdown_time, clear_shutdown_time, set_startup_time, clear_startup_time, getAll, verify_schedule_data
+from .wittyPi import local_tz, wittyPiPath, is_schedule_file_in_use, schedule_file_lines2schedule_file_data, get_schedule_file, process_schedule_data, stringtime2timetuple, calcTime, set_shutdown_time, clear_shutdown_time, set_startup_time, clear_startup_time, getAll, verify_schedule_data, get_input_voltage, get_output_current, get_output_voltage, get_temperature
 #from .runScript import runscript
